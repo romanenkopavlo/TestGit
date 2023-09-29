c3797c438ff2b74f82e02b34276ae91177c07a0a
fforay <fforay@users.noreply.github.com>
2023-09-29 13:17:11 +0200

    Hello

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Bonjour, monsieur Foray");
+
+        System.out.println("Merci Pavlo, bonne journée à toi.");
     }
 }
